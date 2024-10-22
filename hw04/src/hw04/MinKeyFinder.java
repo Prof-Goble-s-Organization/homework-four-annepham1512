3058d7a45bb850a9a9086e98ebe45a190f6ce39c
annepham1512 <phamn@dickinson.edu>
2024-10-22 03:10:20 -0400

    Homework 4 finished!

--- hw04/src/hw04/MinKeyFinder.java
+++ hw04/src/hw04/MinKeyFinder.java
@@ -4,15 +4,24 @@ package hw04;
  * Find the key associated with the minimum value in a tree.
  */
 public class MinKeyFinder implements CS232Visitor<String, Integer> {
+	private String minkey;
+	private Integer minvalue;
 
 
 	public MinKeyFinder() {
+		minkey = null;
+		minvalue = Integer.MAX_VALUE;
+
 	}
 
 	/**
 	 * {@inheritDoc}
 	 */
 	public void visit(String key, Integer value) {
+		if (value <= minvalue) {
+			minkey = key;
+			minvalue = value;
+		}
 
 	}
 
@@ -22,10 +31,26 @@ public class MinKeyFinder implements CS232Visitor<String, Integer> {
 	 * @return the key associated with the minimum value.
 	 */
 	public String getMinKey() {
-		return null;
+		return minkey;
 	}
 	
 	public static void main(String[] args) {
+		CS232LinkedBinaryTree<String, Integer> tree = new CS232LinkedBinaryTree<>();
+		tree.add("A", 5);
+		tree.add("B", 3);
+		tree.add("C", 8);
+		tree.add("D", 1);
+		tree.add("E", 4);
+		tree.add("F", 7);
+		tree.add("G", 10);
+		tree.add("H", 0);
+		tree.add("I", 6);
+		tree.add("J", 9);
+		
+		MinKeyFinder mkf = new MinKeyFinder();
+
+		tree.visitPreOrder(mkf);
+		
 
 	}
 }
